018b110ac59002515fff34d0f4473bb73d7917df
dokspy <danperederenko@gmail.com>
2023-03-22 14:17:31 +0200

    add LoginButton compnent css module

---
+++ src/components/header/LoginButton.jsx
@@ -0,0 +1,9 @@
+import style from './LoginButton.module.css'
+
+export default function LoginButton() {
+    return (
+        <div className={style.reg}>
+            <a href="">Log in</a>
+        </div>
+    )
+}

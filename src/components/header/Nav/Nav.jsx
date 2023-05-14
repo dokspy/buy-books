cc1561c8b063fa277af6b6bae3e3e3036eee5aa1
dokspy <danperederenko@gmail.com>
2023-05-14 16:18:19 +0300

    update code in nav

--- src/components/header/Nav/Nav.jsx
+++ src/components/header/Nav/Nav.jsx
@@ -7,9 +7,9 @@ export default function Nav() {
             <div className={style.dropdown}>
                 <a className={style.dropbtn} href="">Books</a>
                 <div className={style.dropdownContent}>
-                    <a href="/Link">Link 1</a>
-                    <a href="/Link">Link 2</a>
-                    <a href="/Link">Link 3</a>
+                    <Link to='/link1'>Link 1</Link>
+                    <Link to='/link2'>Link 2</Link>
+                    <Link to='/link3'>Link 3</Link>
                 </div>
             </div>
             <div className={style.links}>

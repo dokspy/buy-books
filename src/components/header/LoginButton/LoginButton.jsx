cc24dfc2776e781a269d523e888d001276d79499
dokspy <danperederenko@gmail.com>
2023-05-14 22:25:02 +0300

    add button sign up

--- src/components/header/LoginButton/LoginButton.jsx
+++ src/components/header/LoginButton/LoginButton.jsx
@@ -4,6 +4,7 @@ import style from './LoginButton.module.css'
 export default function LoginButton() {
     return (
         <div className={style.reg}>
+            <Link to="/signup">Sign Up</Link>
             <Link to="/login">Log in</Link>
         </div>
     )
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {Formsignup} from './Formsignup/Formsignup'
 import style from './Signup.module.css'
 
 export const Signup = () => {
     return (
         <div class={style.signup}>
             <h2>Sign Up</h2>
-            <Signup />
+            <Formsignup />
         </div>
     )
 }

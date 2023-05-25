3736283fea00b73d085aa9dbcd92f6084e59deaa
dokspy <danperederenko@gmail.com>
2023-05-25 13:22:31 +0300

    fix spacing

--- src/components/Profile/Saved/book/Book.jsx
+++ src/components/Profile/Saved/book/Book.jsx
@@ -5,11 +5,11 @@ import style from './Book.module.css'
 export const Book = () => {
   return (
     <div className="book">
-      <a href="book/book1.1.txt" target="_blank">
+      <Link to={"book/book1.1.txt"} target="_blank">
         <img src="img/book1.jpg" alt="Книга 1" />
-        <h3>Назва книги 1</h3></a>
+        <h3>Назва книги 1</h3></Link>
       <p>Опис книги 1</p>
-      <a href="#" class="btn">Купити</a>
+      <Link to="#" class="btn">Купити</Link>
     </div>
   )
 }

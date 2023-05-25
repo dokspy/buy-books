dcd51fc80b764e5d76c48025e1cb32e010c3f4cc
dokspy <danperederenko@gmail.com>
2023-05-25 14:11:35 +0300

    add name section Trending

--- src/components/main/TrendingBooks/TrendingBooks.jsx
+++ src/components/main/TrendingBooks/TrendingBooks.jsx
@@ -4,11 +4,16 @@ import styles from './TrendingBooks.module.css'
 export default function TrendingBooks() {
     return (
         <div className={styles.trendingBooks}>
+            <div>
+                <h2>Популярні книги</h2>
+            </div>
+            <div className={styles['trendingBooks-item']}>
             <BookItem name="Example book" />
-            <BookItem name="Example book"/>
-            <BookItem name="Example book"/>
-            <BookItem name="Example book"/>
-            <BookItem name="Example book"/>
+            <BookItem name="Example book" />
+            <BookItem name="Example book" />
+            <BookItem name="Example book" />
+            <BookItem name="Example book" />
+            </div>
         </div>
     )
 }

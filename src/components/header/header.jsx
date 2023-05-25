4a9cccdb12dee2da625f1e77f9ef9b5435bbcc58
dokspy <danperederenko@gmail.com>
2023-05-25 12:26:26 +0300

    add checkAutorithation

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,14 +2,16 @@ import Nav from './Nav/Nav'
 import LoginButton from './LoginButton/LoginButton'
 import LogoItem from './LogoItem/LogoItem'
 import style from'./Header.module.css'
+import { useState } from 'react'
 
 
 const Header = () => {
+    const [checkAutorithation, setCheckAutorithation] = useState(false)
     return (
         <header>
             <LogoItem />
             <Nav />
-            <LoginButton />
+            <LoginButton checkAutorithation={checkAutorithation} />
         </header>
     )
 }

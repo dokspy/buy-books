55596ef1241998170055c62bb81b28969c7330b8
dokspy <danperederenko@gmail.com>
2023-05-25 15:52:35 +0300

    Change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './Footer.module.css'
 
 export const Footer = () => {
-    return(
-        <div>
-            
+    return (
+        <div className={styles['footer']}>
+             <p>© 2023 BuyBook. Усі права захищені.</p>
         </div>
     )
 }

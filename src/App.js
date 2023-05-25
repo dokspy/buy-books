a831bd0a842bd46852b757cade52a83df249d6f1
dokspy <danperederenko@gmail.com>
2023-05-25 18:43:42 +0300

    fix(no) position footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Header from './components/header/Header.jsx';
 import Main from './components/main/Main.jsx';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
@@ -6,21 +5,21 @@ import { Login } from './components/login/Login';
 import { Signup } from './components/signup/Signup';
 import { Profile } from './components/Profile/Profile';
 import { Footer } from './components/Footer/Footer';
+import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Routes>
-          <Route path='/main' element={<Main />}/>
-          <Route  path='/' element={(<Main />)} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/profile' element={<Profile />} />
-          
-        </Routes>
-        <Footer />
+      <div className='tes'>
+      <Header />
+      <Routes>
+        <Route path='/main' element={<Main />} />
+        <Route path='/' element={(<Main />)} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+      <Footer />
       </div>
     </BrowserRouter>
   );

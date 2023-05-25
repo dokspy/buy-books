2a7d069400ea3fe991df6dc67a15d3bc6640e502
dokspy <danperederenko@gmail.com>
2023-05-25 14:46:35 +0300

    add&fis styles for main-panel

--- src/components/main/MainPanel/MainPanel.jsx
+++ src/components/main/MainPanel/MainPanel.jsx
@@ -1,10 +1,12 @@
-import Trend from "./../TrendingBooks/TrendingBooks"
+import RecommendedBooks from '../RecommendedBooks/RecommendedBooks'
+import Trend from './../TrendingBooks/TrendingBooks'
 import styles from './MainPanel.module.css'
 
 const MainPanel = () => {
     return (
         <div className={styles['main-panel']}>
             <Trend />
+            <RecommendedBooks />
         </div>
     )
 }

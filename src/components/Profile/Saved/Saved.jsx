394c7dc4a847b6e55ec2d025d58907f2821ef2ab
dokspy <danperederenko@gmail.com>
2023-05-25 12:24:18 +0300

    add Saved componet

---
+++ src/components/Profile/Saved/Saved.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import { Book } from './book/Book'
+import style from './Saved.module.css'
+
+
+export const Saved = () => {
+    return (
+        <div>
+            <Book />
+            <Book />
+            <Book />
+        </div>
+    )
+}
+
+

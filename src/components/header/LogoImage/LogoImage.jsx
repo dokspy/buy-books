2e765fe40568b8f10d056fcf65f015335c2bbebd
dokspy <danperederenko@gmail.com>
2023-05-25 13:53:06 +0300

    change LogoItem

--- src/components/header/LogoImage/LogoImage.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-import logo5 from './../../../pngs/logo/logo5.png'
-import style from './LogoImage.module.css'
-
-const LogoImage = () => {
-    return <Link to='/main'><img src={logo5} alt="" width={120} /></Link>
-}
-
-export default LogoImage
--- src/components/header/LogoItem/LogoItem.jsx
+++ src/components/header/LogoItem/LogoItem.jsx
@@ -1,9 +1,11 @@
 import LogoImage from './../LogoImage/LogoImage'
+import style from './LogoItem.module.css'
+import { Link } from 'react-router-dom'
 
 export default function LogoItem() {
-    return(
-        <div className="logo">
-            <LogoImage />
-        </div> 
+    return (
+        <div className={style.logo}>
+            <h3><Link to='/'>Buy-Books</Link></h3>
+        </div>
     )
 }

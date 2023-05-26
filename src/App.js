c8f85b2aa8ad00abeba204a31101714a74234fb7
dokspy <danperederenko@gmail.com>
2023-05-26 22:14:40 +0300

    fix import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/header/Header.jsx';
 import Main from './components/main/Main.jsx';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { Login } from './components/login/Login';
-import { Signup } from './components/signup/Signup';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 import { Profile } from './components/Profile/Profile';
 import { Footer } from './components/Footer/Footer';
 import { BookInfo } from './components/BookInfo/BookInfo.jsx';
@@ -12,16 +12,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='tes'>
-      <Header />
-      <Routes>
-        <Route path='/main' element={<Main />} />
-        <Route path='/' element={(<Main />)} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/bookinfo' element={<BookInfo />} />
-      </Routes>
-      <Footer />
+        <Header />
+        <Routes>
+          <Route path='/main' element={<Main />} />
+          <Route path='/' element={(<Main />)} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/bookinfo' element={<BookInfo />} />
+        </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );

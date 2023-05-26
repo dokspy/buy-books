29fc41a6a5abcb467856587a94249323f3488a81
dokspy <danperederenko@gmail.com>
2023-05-26 15:32:04 +0300

    add validation on login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Formlogin } from "./Form/Formlogin";
+
+import { checkEmailValidation, checkPasswordValidation } from '../common/validation/Validation';
+
 import style from './Login.module.css'
-import { Link } from "react-router-dom";
 
 
-export const Login = () => {
+const Login = () => {
+    const [correctField, setCorrectField] = useState( '')
+
+    const [emailValid, setEmailValid] = useState({
+        content: '',
+        status: false,
+
+    });
+    const [passwordValid, setPasswordValid] = useState({
+        content: '',
+        status: false,
+    });
+    const [validContact, setValidContact] = useState({
+        email: '',
+        password: '',
+    })
+
     return (
         <div className={style.login}>
             <h2>Login</h2>
-            <Formlogin />
-            
+            <Formlogin
+                // signIn={signIn}
+                validContact={validContact}
+                setValidContact={setValidContact}
+                checkEmailValidation={checkEmailValidation}
+                checkPasswordValidation={checkPasswordValidation}
+                emailValid={emailValid}
+                correctField={correctField}
+                setEmailValid={setEmailValid}
+                passwordValid={passwordValid}
+                setPasswordValid={setPasswordValid}
+                // emailError={userLogError.email}
+                // passwordError={userLogError.password}
+                // userLogStatus={userLogStatus}
+            />
+
         </div>
     )
 }
+
+
+export default Login;

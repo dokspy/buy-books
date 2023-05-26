35512d5017b368f37d89db471344d1fc57f728fc
dokspy <danperederenko@gmail.com>
2023-05-26 12:27:23 +0300

    add draft validation code

---
+++ src/components/common/validation/Validation.js
@@ -0,0 +1,48 @@
+export const checkEmailValidation = (setValidContact, setEmailValid, validContact) => {
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    if (validContact.email.length <= 0) {
+      return setEmailValid({ content: '', status: false })
+    }
+    if (validContact.email.match(validRegex)) {
+      return setEmailValid({ content: '', status: true });
+    } else {
+      return setEmailValid({ content: 'Invalid email address!', status: false });
+    }
+  }
+  
+  export const checkFormLength = (setAcceptOrder, setCorrectField, emailStatus, validContact, passwordStatus) => {
+    const objectValues = Object.values(validContact);
+    const objectKeys = Object.keys(validContact);
+    let status = false;
+  
+    objectKeys?.map((el, id) => {
+      objectValues?.map((item) => {
+        if ((validContact[el].length > 0) && (item[id].length > 0)) {
+          if (emailStatus || (emailStatus && passwordStatus)) {
+            setCorrectField('')
+            status = true
+            setAcceptOrder(true)
+          }
+        } else {
+          setCorrectField('This field is empty')
+          status = false
+        }
+      })
+    })
+    return status
+  }
+  
+  export const checkPasswordValidation = (setPasswordValid, value) => {
+    const validRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  
+    if (value.length <= 0) {
+      return setPasswordValid({ content: '', status: false })
+    }
+  
+    if (value.match(validRegex)) {
+      return setPasswordValid({ content: '', status: true });
+    } else {
+      return setPasswordValid({ content: 'Minimum eight characters, at least one letter and one number', status: false });
+    }
+  }

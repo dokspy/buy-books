5b94f59f94877f6cc855b13fd24688b658fa8d82
dokspy <danperederenko@gmail.com>
2023-07-06 12:15:43 +0300

    delete files for refactor

--- src/components/main/image.jsx
+++
@@ -1,6 +0,0 @@
-import expml from './../../pngs/exmpl.png'
-
-export const Image = ( { bookPhoto } ) => {
-    return <img src={bookPhoto} alt="" width={150} height={200} />
-}
-
--- src/components/main/main.jsx
+++
@@ -1,9 +0,0 @@
-import MainPanel from './MainPanel/MainPanel'
-
-const Main = () => {
-    return (
-        <MainPanel />           
-    )
-}
-
-export default Main
